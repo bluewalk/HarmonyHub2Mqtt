c8de7e49232699f3193c94a1245f311e44edd487
Delano REIJNIERSE <delano.reijnierse@zrefinery.nl>
2018-12-21 09:10:50 +0100

    Polishing before first release

--- Net.Bluewalk.HarmonyHub2Mqtt.Service/Service.cs
+++ Net.Bluewalk.HarmonyHub2Mqtt.Service/Service.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
+using System.Configuration.Install;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
